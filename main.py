4e407373077297b3143712a05c1a89cfdc8552c8
Senh Mo Chuang <senhmo@Senhs-MacBook-Pro.local>
2020-04-03 20:09:44 -0500

    added features of aiohttp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import aiohttp
 import asyncio
 import time
+import ujson
 
 def timeit_wrapper(func):
     def wrapper(*args, **kwargs):
@@ -15,18 +16,73 @@ def timeit_wrapper(func):
 async def fetch(session, url):
     async with session.get(url) as response:
     # async with session.get(url, ssl=ssl.SSLContext()) as response:
+    # async with aiohhtp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False))
         # return await response.json()
+
+        # with open(filename, 'wb') as fd:
+        # while True:
+        #     chunk = await resp.content.read(chunk_size)
+        #     if not chunk:
+        #         break
+        #     fd.write(chunk)
+
         return await response.text()
 
 async def main():
     async with aiohttp.ClientSession() as session:
         html1 = await fetch(session, 'http://google.com')
-
-    # async with aiohttp.ClientSession() as session:
         html2 = await fetch(session, 'http://python.org')
+        html2 = await fetch(session, 'http://cnn.com')
+
+    # async with aiohttp.ClientSession(json_serialize=ujson.dumps) as session:
+    #     async with session.post(json={'test': 'object})
+    #     async with session.put('http://httpbin.org/put', data=b'data')
+    #     async with session.post('http://httpbin.org/post', data=b'data')
+    #     async with session.delete('http://httpbin.org/delete')
+    #     async with session.head('http://httpbin.org/get')
+    #     async with session.options('http://httpbin.org/get')
+    #     async with session.patch('http://httpbin.org/patch', data=b'data')
+
+    #     params = {'key1': 'value1', 'key2': 'value2'}
+    #     async with session.get('http://httpbin.org/get',params=params) as resp:
+    #         assert str(resp.url) == 'http://httpbin.org/get?key2=value2&key1=value1'
+        
+    #     params = [('key', 'value1'), ('key', 'value2')]
+    #     async with session.get('http://httpbin.org/get', params=params) as r:
+    #         assert str(r.url) == 'http://httpbin.org/get?key=value2&key=value1'
+
+    #     url = 'https://api.github.com/some/endpoint'
+    #     payload = {'some': 'data'}
+    #     headers = {'content-type': 'application/json'}
+    #     await session.post(url, data=ujson.dumps(payload), headers=headers)
+
+    #     payload = {'key1': 'value1', 'key2': 'value2'}
+    #     async with session.post('http://httpbin.org/post', data=payload) as resp:
+    #         print(await resp.text())
+
+    #     url = 'http://httpbin.org/post'
+    #     files = {'file': open('report.xls', 'rb')}
+    #     await session.post(url, data=files)
 
     # async with aiohttp.ClientSession() as session:
-        html2 = await fetch(session, 'http://cnn.com')
+    #     async with session.get("http://python.org",
+    #                         proxy="http://some.proxy.com") as resp:
+    #         print(resp.status)
+
+    # async with aiohttp.ClientSession(
+    #     headers={"Authorization": "Basic bG9naW46cGFzcw=="}) as session:
+    #     async with session.get("http://httpbin.org/headers") as r:
+    #         json_body = await r.json()
+    #         assert json_body['headers']['Authorization'] == \
+    #             'Basic bG9naW46cGFzcw=='
+
+    # url = 'http://httpbin.org/cookies'
+    # cookies = {'cookies_are': 'working'}
+    # async with ClientSession(cookies=cookies) as session:
+    #     async with session.get(url) as resp:
+    #         assert await resp.json() == {
+    #         "cookies": {"cookies_are": "working"}}
+
 
 async def fetch_all(urls, loop):
     async with aiohttp.ClientSession(loop=loop) as session:
